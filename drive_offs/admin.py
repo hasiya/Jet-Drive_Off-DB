7f224d9053e8f01d844c10e350ef0c1ed4da32e6
hasiya <hasith002@yahoo.com>
2020-08-21 12:04:55 +0100

    Added Paid and Police Ref to the model

--- drive_offs/admin.py
+++ drive_offs/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 
 from .models import DriveOff
 
+
 class DriveOffAdmin(admin.ModelAdmin):
-    list_display = ['date_time', 'reg_no','transaction_amount']
+    list_display = ['date_time', 'reg_no', 'transaction_amount', 'paid','staff_name']
     list_filter = ['date_time']
     search_fields = ['reg_no']
+    date_hierarchy = 'date_time'
+    fields = ['date_time', ('reg_no','paid'), 'vehicle_type', 'vehicle_make', 'vehicle_colour',
+              'transaction_amount', 'staff_name', 'police_reference', 'additional_notes']
 
-admin.site.register(DriveOff,DriveOffAdmin);
+admin.site.register(DriveOff, DriveOffAdmin)
--- drive_offs/models.py
+++ drive_offs/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 
+
 class DriveOff(models.Model):
     date_time = models.DateTimeField('date and time')
-    reg_no = models.CharField('Registration no',max_length=20)
-    vehicle_type = models.CharField('vehicle type (eg: Car)',max_length=50,blank=True)
-    vehicle_make = models.CharField(max_length=100,blank=True)
-    vehicle_colour = models.CharField(max_length=50,blank=True)
-    transaction_amount = models.DecimalField("Transaction Amount (£)",max_digits=6, decimal_places=2)
+    reg_no = models.CharField('Registration no', max_length=20)
+    paid = models.BooleanField("Paid?")
+    vehicle_type = models.CharField(
+        'vehicle type (eg: Car)', max_length=50, blank=True)
+    vehicle_make = models.CharField(max_length=100, blank=True)
+    vehicle_colour = models.CharField(max_length=50, blank=True)
+    transaction_amount = models.DecimalField(
+        "Transaction amount (£)", max_digits=6, decimal_places=2)
     staff_name = models.CharField(max_length=50)
+    police_reference = models.CharField(
+        'police reference no:', max_length=20, blank=True)
     additional_notes = models.TextField(blank=True)
 
+    def save(self, *args, **kwargs):
+        self.reg_no = self.reg_no.upper()
+        super().save(*args, **kwargs)
+
     class Meta:
-        ordering = ['date_time']
-        
+        ordering = ['-date_time']
+
     def __str__(self):
         return self.reg_no
-
